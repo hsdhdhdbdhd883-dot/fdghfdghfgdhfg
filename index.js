f571a2c68ad11e1dea5a92325da731a7ffea3680
hsdhdhdbdhd883-dot <hsdhdhdbdhd883@gmail.com>
2026-01-29 16:39:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -753,7 +753,8 @@ app.get('/', (req, res) => {
     `);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server: http://localhost:${PORT}`);
+
 });
